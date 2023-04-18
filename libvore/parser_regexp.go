d41c7e204baeb9fa337643c65b96e93206b97a85
jmeaster30 <jmeasterday@comcast.net>
2023-04-17 21:46:12 -0400

    Moved the quantifier parsing code

--- libvore/parser_regexp.go
+++ libvore/parser_regexp.go
@@ -92,70 +92,15 @@ func parse_regexp_pattern(regexp_token *Token, regexp string, index int) (AstExp
 		return nil, next_index, err
 	}
 
-	if next_index >= len(regexp) {
-		return &AstPrimary{start}, next_index, nil
-	}
-
-	op := regexp[next_index]
-	var end_idx int
-	var exp *AstLoop
-	if op == '*' {
-		exp = &AstLoop{0, -1, false, &AstPrimary{start}, ""}
-		end_idx = next_index + 1
-	} else if op == '+' {
-		exp = &AstLoop{1, -1, false, &AstPrimary{start}, ""}
-		end_idx = next_index + 1
-	} else if op == '?' {
-		exp = &AstLoop{0, 1, false, &AstPrimary{start}, ""}
-		end_idx = next_index + 1
-	} else if op == '{' {
-		from, idx, err := parse_regexp_number(regexp_token, regexp, next_index+1)
-		if err != nil {
-			return nil, idx, err
-		}
-		comma_or_brace := regexp[idx]
-
-		if comma_or_brace == ',' {
-			if regexp[idx+1] == '}' {
-				exp = &AstLoop{from, -1, false, &AstPrimary{start}, ""}
-				end_idx = idx + 2
-			} else {
-				to, idx2, err := parse_regexp_number(regexp_token, regexp, idx+1)
-				if err != nil {
-					return nil, idx, err
-				}
-				brace := regexp[idx2]
-				if brace != '}' {
-					return nil, idx2, NewParseError(*regexp_token, "Unexpected character. Expected '}'")
-				}
-
-				exp = &AstLoop{from, to, false, &AstPrimary{start}, ""}
-				end_idx = idx2 + 1
-			}
-		} else if comma_or_brace == '}' {
-			exp = &AstLoop{from, from, false, &AstPrimary{start}, ""}
-			end_idx = idx + 1
-		}
-	} else if op == '|' {
-		end, idx, err := parse_regexp_pattern(regexp_token, regexp, next_index+1)
-		return &AstBranch{start, end}, idx, err
-	} else {
-		return &AstPrimary{start}, next_index, nil
-	}
-	exp.fewest = end_idx < len(regexp) && regexp[end_idx] == '?'
-	if exp.fewest {
-		end_idx += 1
-	}
-
-	if end_idx < len(regexp) {
-		if regexp[end_idx] == '|' {
-			end, idx, err := parse_regexp_pattern(regexp_token, regexp, end_idx+1)
-			return &AstBranch{&AstSubExpr{[]AstExpression{exp}}, end}, idx, err
+	if next_index < len(regexp) {
+		if regexp[next_index] == '|' {
+			end, idx, err := parse_regexp_pattern(regexp_token, regexp, next_index+1)
+			return &AstBranch{&AstSubExpr{[]AstExpression{start}}, end}, idx, err
 		} else {
-			return exp, end_idx, nil
+			return start, next_index, nil
 		}
 	} else {
-		return exp, end_idx, nil
+		return start, next_index, nil
 	}
 }
 
@@ -176,41 +121,120 @@ func parse_regexp_number(regexp_token *Token, regexp string, index int) (int, in
 		return value, idx, NewParseError(*regexp_token, "Error converting string to number")
 	}
 	return value, idx, nil
-
 }
 
-func parse_regexp_literal(regexp_token *Token, regexp string, index int) (AstLiteral, int, error) {
+func parse_regexp_literal(regexp_token *Token, regexp string, index int) (AstExpression, int, error) {
 	c := regexp[index]
 	var start AstLiteral
 	next_index := index
 	if c == '^' {
 		start = &AstCharacterClass{false, ClassLineStart}
 		next_index += 1
+		return &AstPrimary{start}, next_index, nil
 	} else if c == '$' {
 		start = &AstCharacterClass{false, ClassLineEnd}
 		next_index += 1
+		return &AstPrimary{start}, next_index, nil
 	} else if c == '\\' {
 		exp, idx, err := parse_regexp_escape_characters(regexp_token, regexp, index+1)
 		if err != nil {
 			return nil, index, err
 		}
-		start = exp
-		next_index = idx
+		return &AstPrimary{exp}, idx, nil
 	} else if c == '(' {
-		exp, idx, err := parse_regexp_groups(regexp_token, regexp, index+1)
+		start, next_index, err := parse_regexp_groups(regexp_token, regexp, index+1)
 		if err != nil {
-			return nil, index, err
+			return nil, next_index, err
 		}
-		start = exp
-		next_index = idx
+		exp, idx, err := parse_regexp_quantifier(regexp_token, regexp, next_index)
+		if err != nil {
+			return nil, idx, err
+		}
+		if exp == nil {
+			return &AstPrimary{start}, idx, nil
+		}
+		exp.body = &AstPrimary{start}
+		return exp, idx, nil
 	} else if c == '.' {
 		start = &AstString{true, "\n", false}
 		next_index += 1
+		exp, idx, err := parse_regexp_quantifier(regexp_token, regexp, next_index)
+		if err != nil {
+			return nil, idx, err
+		}
+		if exp == nil {
+			return &AstPrimary{start}, idx, nil
+		}
+		exp.body = &AstPrimary{start}
+		return exp, idx, nil
 	} else {
 		start = &AstString{false, string(c), false}
 		next_index += 1
+		exp, idx, err := parse_regexp_quantifier(regexp_token, regexp, next_index)
+		if err != nil {
+			return nil, idx, err
+		}
+		if exp == nil {
+			return &AstPrimary{start}, idx, nil
+		}
+		exp.body = &AstPrimary{start}
+		return exp, idx, nil
 	}
-	return start, next_index, nil
+}
+
+func parse_regexp_quantifier(regexp_token *Token, regexp string, index int) (*AstLoop, int, error) {
+	if index >= len(regexp) {
+		return nil, index, nil
+	}
+	op := regexp[index]
+	var end_idx int
+	var exp *AstLoop
+	if op == '*' {
+		exp = &AstLoop{0, -1, false, nil, ""}
+		end_idx = index + 1
+	} else if op == '+' {
+		exp = &AstLoop{1, -1, false, nil, ""}
+		end_idx = index + 1
+	} else if op == '?' {
+		exp = &AstLoop{0, 1, false, nil, ""}
+		end_idx = index + 1
+	} else if op == '{' {
+		from, idx, err := parse_regexp_number(regexp_token, regexp, index+1)
+		if err != nil {
+			return nil, idx, err
+		}
+		comma_or_brace := regexp[idx]
+
+		if comma_or_brace == ',' {
+			if regexp[idx+1] == '}' {
+				exp = &AstLoop{from, -1, false, nil, ""}
+				end_idx = idx + 2
+			} else {
+				to, idx2, err := parse_regexp_number(regexp_token, regexp, idx+1)
+				if err != nil {
+					return nil, idx, err
+				}
+				brace := regexp[idx2]
+				if brace != '}' {
+					return nil, idx2, NewParseError(*regexp_token, "Unexpected character. Expected '}'")
+				}
+
+				exp = &AstLoop{from, to, false, nil, ""}
+				end_idx = idx2 + 1
+			}
+		} else if comma_or_brace == '}' {
+			exp = &AstLoop{from, from, false, nil, ""}
+			end_idx = idx + 1
+		}
+	} else {
+		exp = nil
+		end_idx = index
+	}
+	exp.fewest = end_idx < len(regexp) && regexp[end_idx] == '?'
+	if exp.fewest {
+		end_idx += 1
+	}
+	return exp, end_idx, nil
 }
 
 func parse_regexp_escape_characters(regexp_token *Token, regexp string, index int) (AstLiteral, int, error) {
